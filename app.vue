9fd77868423f6c77d1f2c66bf761855fa15ab4db
Vlad Makarov <makvv4@gmail.com>
2023-03-31 00:18:40 +0700

    changes

--- app.vue
+++ app.vue
@@ -25,11 +25,10 @@
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Stand out from the crowd with
             UNIQUE YOU - where every beauty is unique</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Stand out from the crowd with
-            UNIQUE YOU - where every beauty is unique</p>
+          <p class="mt-6 text-lg leading-8 text-gray-600"></p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="https://b780875.alteg.io" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Order service</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Instagram <span aria-hidden="true">→</span></a>
+            <a href="https://instagram.com/unique_you_dubai?igshid=YmMyMTA2M2Y=" class="text-sm font-semibold leading-6 text-gray-900">Instagram <span aria-hidden="true">→</span></a>
           </div>
         </div>
       </div>
