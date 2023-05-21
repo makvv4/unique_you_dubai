45c4b203b34ce3fa575085e85e8187f59f27dffe
Vlad Makarov <makvv4@gmail.com>
2023-05-22 00:13:42 +0700

    improve ui

--- app.vue
+++ app.vue
@@ -1,145 +1,7 @@
 <template>
   <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
-      <nav>
-        <div
-          class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
-        >
-          <a href="#" class="flex items-center">
-            <span
-              class="self-center text-2xl font-semibold whitespace-nowrap dark:text-myPrimary"
-              >UNIQUE</span
-            >
-          </a>
-          <div class="link text-gray-700 cursor-pointer hover:underline hover:text-black-950"><a href="/unique_pricing.pdf" target="_blank">Pricing</a></div>
-          <div
-            class="flex flex-row content-center items-center space-x-6 sm:justify-center sm:mt-0"
-          >
-            <a
-              href="https://www.facebook.com/uniqueyoudubai?mibextid=LQQJ4d"
-              class="text-myPrimary hover:text-pink-500 cursor-pointer"
-            >
-              <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-            <a
-              href="https://instagram.com/unique_you_downtown?igshid=YmMyMTA2M2Y="
-              class="text-myPrimary hover:text-pink-500 cursor-pointer"
-            >
-              <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-            <a
-              href="tel:+971508008462"
-              class="flex content-center items-center text-myPrimary hover:underline hover:text-pink-500 cursor-pointer"
-              ><Icon name="heroicons:device-phone-mobile-20-solid"
-            /></a>
-          </div>
-        </div>
-      </nav>
-    </header>
-
-    <div class="relative isolate px-6 pt-14 lg:px-8">
-      <div
-        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style="
-            clip-path: polygon(
-              74.1% 44.1%,
-              100% 61.6%,
-              97.5% 26.9%,
-              85.5% 0.1%,
-              80.7% 2%,
-              72.5% 32.5%,
-              60.2% 62.4%,
-              52.4% 68.1%,
-              47.5% 58.3%,
-              45.2% 34.5%,
-              27.5% 76.7%,
-              0.1% 64.9%,
-              17.9% 100%,
-              27.6% 76.8%,
-              76.1% 97.7%,
-              74.1% 44.1%
-            );
-          "
-        />
-      </div>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="text-center">
-          <h1
-            class="text-4xl font-bold tracking-tight text-gray-700 sm:text-6xl"
-          >
-            Stand out from the crowd with UNIQUE YOU - where every beauty is
-            unique
-          </h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600"></p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="https://b780875.alteg.io"
-              class="rounded-md bg-mySecondary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >Order service</a
-            >
-            <a
-              href="https://instagram.com/unique_you_downtown?igshid=YmMyMTA2M2Y="
-              class="text-sm font-semibold leading-6 text-myPrimary hover:underline hover:text-pink-500"
-              >Instagram <span aria-hidden="true">→</span></a
-            >
-          </div>
-        </div>
-      </div>
-      <div
-        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style="
-            clip-path: polygon(
-              74.1% 44.1%,
-              100% 61.6%,
-              97.5% 26.9%,
-              85.5% 0.1%,
-              80.7% 2%,
-              72.5% 32.5%,
-              60.2% 62.4%,
-              52.4% 68.1%,
-              47.5% 58.3%,
-              45.2% 34.5%,
-              27.5% 76.7%,
-              0.1% 64.9%,
-              17.9% 100%,
-              27.6% 76.8%,
-              76.1% 97.7%,
-              74.1% 44.1%
-            );
-          "
-        />
-      </div>
-    </div>
+    <Header />
+    <HeroSection />
 
     <div
       class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8 isolate"
@@ -149,51 +11,22 @@
           Welcome to our salon
         </h2>
         <p class="mt-4 text-gray-700">
-          UNIQUE YOU is a beautiful and cozy beauty salon located in Dubai, UAE.
-          We offer a wide range of services to meet our clients' beauty and
-          self-care needs. Our team of professionals provides services for hair
-          care, manicures and pedicures, eyelash extensions, eyebrows,permament
-          makeup, microblading and shading. We strive to meet the needs of each
-          client, and our team works hard to provide high- quality service and
-          results to be proud of.
+            Do you have a passion for beauty and a dream of creating a successful business in this area?
+            We invite you to become a part of the recognizable brand Unique You Beauty Salon!
+            We believe that everyone deserves luxury and quality service, but not everyone can afford it.
+            That is why we have created a salon where you can get VIP service at affordable prices.
+            We will help you open your own beauty salon based on our successful brand and share with you our knowledge and experience in this field.
+            Contact us via our corporate email to learn more about our franchise program
+            <a class="text-mySecondary" href="mailto:unique.you.cooperation@gmail.com">unique.you.cooperation@gmail.com</a>
         </p>
-
-        <dl
-          class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8"
-        >
-          <div
-            v-for="feature in features"
-            :key="feature.name"
-            class="border-t border-gray-200 pt-4"
-          >
-            <dt class="font-medium text-gray-900">{{ feature.name }}</dt>
-            <dd class="mt-2 text-sm text-gray-500">
-              {{ feature.description }}
-            </dd>
-          </div>
-        </dl>
       </div>
-      <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-        <img
-          src="/img/1.jpg"
-          alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-          class="rounded-lg bg-gray-100"
-        />
-        <img
-          src="/img/2.jpg"
-          alt="Top down view of walnut card tray with embedded magnets and card groove."
-          class="rounded-lg bg-gray-100"
-        />
-        <img
-          src="/img/3.jpg"
-          alt="Side of walnut card tray with card groove and recessed card area."
-          class="rounded-lg bg-gray-100"
-        />
-        <img
-          src="/img/4.jpg"
-          alt="Walnut card tray filled with cards and card angled in dedicated groove."
-          class="rounded-lg bg-gray-100"
-        />
+      <div class="grid grid-cols-2 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
+        <video autoplay loop muted playsinline class="rounded-lg bg-gray-100">
+            <source src="~/assets/videos/unique_video1.mp4" type="video/mp4">
+        </video>
+        <video autoplay class="rounded-lg bg-gray-100">
+            <source src="~/assets/videos/unique_video2.mp4" type="video/mp4">
+        </video>
       </div>
       <div
         class="absolute inset-x-0 -top-100 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-100"
@@ -225,110 +58,13 @@
       </div>
     </div>
 
-    <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
-      <div class="mx-auto max-w-screen-xl">
-        <div class="md:flex md:justify-between">
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2
-                class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
-              >
-                Dubai
-              </h2>
-              <ul class="text-gray-600 dark:text-gray-400">
-                <li class="mb-4">
-                  Business Bay, Crystal Tower, Exponenta Business Center, M1
-                  floor, Shop 7 Dubai, United Arab Emirates
-                </li>
-                <li class="flex flex-row content-center items-center">
-                  <Icon name="heroicons:device-phone-mobile" />
-                  <a href="tel:+971508008462" class="ml-2 hover:underline"
-                    >+971 50 800 8462</a
-                  >
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2
-                class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"
-              >
-                Legal
-              </h2>
-              <ul class="text-gray-600 dark:text-gray-400">
-                <li class="mb-4">
-                  <a href="#" class="hover:underline">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr
-          class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"
-        />
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-            >© 2023
-            <a href="#" class="hover:underline">Aleksandra Beauty Saloon LLC</a
-            >. All Rights Reserved.
-          </span>
-          <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a
-              href="https://www.facebook.com/uniqueyoudubai?mibextid=LQQJ4d"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-            <a
-              href="https://instagram.com/unique_you_downtown?igshid=YmMyMTA2M2Y="
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
-import PricingModal from "~/components/PricingModal.vue";
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
-
-const mobileMenuOpen = ref(false)
+import HeroSection from "~/components/HeroSection.vue";
+import Footer from "~/components/Footer.vue";
 
 useHead({
   title: 'UNIQUE',
---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="absolute inset-x-0 top-0 z-50">
+        <nav>
+            <div
+                    class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
+            >
+                <a href="#" class="flex items-center">
+            <span
+                    class="self-center text-2xl font-semibold whitespace-nowrap dark:text-myPrimary"
+            >UNIQUE</span
+            >
+                </a>
+                <div class="link text-gray-700 cursor-pointer hover:underline hover:text-black-950"><a href="/unique_pricing.pdf" target="_blank">Pricing</a></div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
